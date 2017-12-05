c1380d0d3f01012853d988530f784ff70992e168
Pablo Hidalgo Martínez <21647189@live.uem.es>
2017-12-05 18:02:39 +0100

    Update Commit

--- src/control/ControlFX.java
+++ src/control/ControlFX.java
@@ -1,5 +1,26 @@
 package control;
 
-public class ControlFX {
-	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class ControlFX extends Application{
+	private Stage stage;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		this.stage = primaryStage;
+	}
+	public void loadNewScene() {
+		try {
+		AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("../vista/FreeSolo.fxml"));
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(getClass().getResource("../vista/assets/styles/styles.css").toExternalForm());
+		stage.setScene(scene);
+		stage.show();
+		} catch (Exception e) {
+	}
+	}
 }
